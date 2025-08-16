92db30431fbb0def7593a0815cc007059bbffd75
drondistortion <cancelbutton@gmail.com>
2025-08-16 20:51:00 +0300

    more workflows BS

--- system.sh
+++ system.sh
@@ -53,5 +53,5 @@ echo "export RK_BOOTARGS_CMA_SIZE=\"1M\"" >> .BoardConfig.mk
 popd || exit
 
 rm -rf output
-mkdir -p output
+mkdir -p output/image
 cp sdk/output/image/update.img "output/$DEVICE_NAME-sysupgrade.img"
